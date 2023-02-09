2304c86241b65b5bc2d19a2413c8ae534d971761
Maciej Plachta <macpla08@gmail.com>
2023-02-09 18:27:33 +0100

    Eddited routing and index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $path = parse_url($path, PHP_URL_PATH);
 
 Router::get('', 'DefaultController');
 Router::get('projects', 'ProjectController');
-#Router::post('login', 'SecurityController');
+Router::post('login', 'SecurityController');
 #Router::post('addProject', 'ProjectController');
 #Router::post('register', 'SecurityController');
 #Router::post('search', 'ProjectController');
